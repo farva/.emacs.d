d5d97843efe613772f4364e3904e2793843ac89f
farva <orenbaracha@gmail.com>
2019-02-11 23:09:38 +0200

    initial commit

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-by-moving-to-trash t)
+ '(lsp-ui-doc-enable nil)
+ '(lsp-ui-sideline-show-hover nil)
+ '(lsp-ui-sideline-show-symbol nil)
+ '(package-selected-packages
+   (quote
+    (multi-term company-quickhelp ace-link aggressive-indent aggresive-indent elpy eglot yasnippet gotest lsp-mode smex yaml-mode counsel-projectile magit rainbow-delimiters evil-commentary dashboard request restclient lispyville smartparens swiper counsel ivy avy projectile quelpa-use-package quelpa evil-surround go-mode flycheck general which-key doom-themes evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 151 :width normal)))))
---
+++ init.el
@@ -0,0 +1,436 @@
+;; detach customizations to a seperate file
+(setq custom-file (locate-user-emacs-file "custom.el"))
+
+;; Minimal UI
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+;; "sane defaults" - https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+(setq delete-old-versions -1 )		; delete excess backup versions silently
+(setq version-control t )		; use version control
+(setq vc-make-backup-files t )		; make backups file even when in version controlled dir
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory (file-name-as-directory "backups"))))) ; which directory to put backups file
+(setq vc-follow-symlinks t )				       ; don't ask for confirmation when opening symlinked file
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory (file-name-as-directory "auto-save-list")) t))) ;transform backups file name
+(setq inhibit-startup-screen t )	; inhibit useless and old-school startup screen
+(setq ring-bell-function 'ignore )	; silent bell when you make a mistake
+(setq coding-system-for-read 'utf-8 )	; use utf-8 by default
+(setq coding-system-for-write 'utf-8 )
+(setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
+(setq-default fill-column 80)		; toggle wrapping text at the 80th character
+(save-place-mode)			  ; save position in files
+(show-paren-mode)			  ; show matching delimiter
+
+;; Package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Quelpa
+(use-package quelpa
+  :ensure t
+  :init (setq quelpa-update-melpa-p nil))
+
+;; Quelpa handler for `use-package'
+(use-package quelpa-use-package
+  :ensure t)
+
+;; Vim mode
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+  ;; finer `undo' while in insert mode
+  (setq evil-want-fine-undo t))
+
+;; `evil' integration with other packages
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :init
+  (setq evil-collection-setup-minibuffer t)
+  (setq evil-collection-outline-bind-tab-p nil) 
+  :config
+  (evil-collection-init))
+
+;; Vim mode surround functionality
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+;; Theme
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
+
+;; Which Key
+(use-package which-key
+  :ensure t
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+;; avy
+(use-package avy :ensure t
+  :commands (avy-goto-word-1))
+
+;; Smex (for ivy?)
+(use-package smex
+  :ensure t)
+
+;; ivy
+(use-package ivy
+  :ensure t
+  ;; :commands (ivy-switch-buffer)
+  :after smex
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
+;; counsel
+(use-package counsel
+  :ensure t
+  ;; :commands (counsel-M-x counsel-find-file counsel-describe-variable counsel-describe-function)
+  )
+
+;; swiper
+(use-package swiper
+  :ensure t
+  ;; :commands (swiper)
+  )
+
+;; Company mode
+(use-package company
+  :ensure t
+  :init
+  ;; No delay in showing suggestions.
+  (setq company-idle-delay 0)
+  ;; Show suggestions after entering one character.
+  (setq company-minimum-prefix-length 1)
+  (setq company-selection-wrap-around t)
+  :hook (after-init . global-company-mode))
+
+(use-package company-quickhelp
+  :disabled
+  :ensure t
+  :after company
+  :config
+  (company-quickhelp-mode)
+  ;; (define-key company-active-map (kbd "C-c h") #'company-quickhelp-manual-begin)
+  )
+
+;; Custom keybinding
+(use-package general
+  :ensure t
+  :init
+  (setq general-override-states '(insert
+                                  emacs
+                                  hybrid
+                                  normal
+                                  visual
+                                  motion
+                                  operator
+                                  replace))
+  :config
+  (general-define-key
+   ;; replace default keybindings
+   "C-s" 'counsel-grep-or-swiper ; search for string in current buffer
+   "M-x" 'counsel-M-x            ; replace default M-x with ivy backend
+   ;; "TAB" 'indent-for-tab-command ; replace default with 'smart' one
+   )
+  (general-define-key
+   :states '(normal visual motion insert emacs)
+   :keymaps 'override
+   :prefix "SPC"
+   :non-normal-prefix "M-SPC"
+   ;; "/"   '(counsel-rg :which-key "ripgrep") ; You'll need counsel package for this
+   "TAB" '(spacemacs/alternate-buffer :which-key "previous buffer")
+   "SPC" '(counsel-M-x :which-key "M-x")
+   "P" 'counsel-yank-pop ; kill ring
+   ;; Jumping
+   "j" '(:ignore t :which-key "jump")
+   "jw" '(avy-goto-word-1 :which-key "word")
+   "jl" '(link-hint-open-link :which-key "link")
+   ;; Files
+   "f" '(:ignore t :which-key "files")
+   "ff" '(counsel-find-file :which-key "find files")
+   "fe" '(find-user-init-file :which-key "init file")
+   ;; Buffers
+   "b" '(:ignore t :which-key "buffers")
+   "bb" '(ivy-switch-buffer :which-key "switch buffer")
+   "b0" '(kill-buffer-and-window :which-key "kill buffer and window")
+   ;; Window
+   "w" '(:ignore t :which-key "windows")
+   "wl"  '(windmove-right :which-key "move right")
+   "wh"  '(windmove-left :which-key "move left")
+   "wk"  '(windmove-up :which-key "move up")
+   "wj"  '(windmove-down :which-key "move bottom")
+   "w/"  '(split-window-right :which-key "split right")
+   "w-"  '(split-window-below :which-key "split bottom")
+   "w0"  '(delete-window :which-key "delete window")
+   "w1"  '(delete-other-windows :which-key "maximize window")
+   ;; Quit
+   "q" '(:ignore t :which-key "quit")
+   "qq" '(save-buffers-kill-terminal :which-key "quit client")
+   ;; Help
+   "h" '(:ignore t :which-key "help")
+   "hv" '(counsel-describe-variable :which-key "describe variable")
+   "hf" '(counsel-describe-function :which-key "describe function")
+   "hk" '(describe-key :which-key "describe key")
+   "hc" '(describe-key-briefly :which-key "show key binding")
+   "hb" '(counsel-descbinds :which-key "describe bindings")
+   ;; Projectile
+   "p" '(projectile-command-map :which-key "projects")
+   ;; Applications
+   "a" '(:ignore t :which-key "apps")
+   "at"  '(ansi-term :which-key "open terminal")
+   ;; git
+   "g" '(:ignore t :which-key "git")
+   "gs" '(magit-status :which-key "status")))
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'")
+
+(use-package lsp-mode
+  ;; :quelpa (lsp-mode :fetcher github :repo "farva/lsp-mode" :branch "redundant-client")
+  :ensure t
+  :commands lsp
+  :init
+  (setq lsp-clients-go-server "go-langserver")
+  :hook (go-mode . lsp))
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui)
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp
+  :after company
+  :init (push 'company-lsp company-backends))
+
+(use-package eglot
+  :disabled
+  :ensure t
+  :hook (go-mode . eglot-ensure)
+  ;; :init
+  ;; (with-eval-after-load 'eglot
+  ;;   (assq-delete-all 'go-mode eglot-server-programs)
+  ;;   (add-to-list 'eglot-server-programs
+  ;; 		 '(go-mode . ("bingo" "-mode=stdio"))))
+  )
+
+;; Projectile
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1))
+
+(use-package counsel-projectile
+  :ensure t
+  :after (:all counsel projectile)
+  :config
+  (counsel-projectile-mode))
+
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown")
+  :config (setq markdown-header-scaling t))
+
+;; smartparens
+(use-package smartparens
+  :ensure t
+  :commands (smartparens-strict-mode smartparens-mode)
+  :init (add-hook 'prog-mode-hook (lambda ()
+				    (if (derived-mode-p 'sh-mode)
+					(smartparens-mode)
+				      (smartparens-strict-mode))))
+  :config
+  (require 'smartparens-config))
+
+;; lispyvill
+(use-package lispyville
+  :ensure t
+  :hook (prog-mode . lispyville-mode)
+  :config
+  (with-eval-after-load 'evil-commentary
+    (add-to-list 'evil-commentary-comment-function-for-mode-alist '(emacs-lisp-mode . lispyville-comment-or-uncomment))
+    (add-to-list 'evil-commentary-comment-function-for-mode-alist '(lisp-mode . lispyville-comment-or-uncomment))))
+
+;; REST-client
+(use-package restclient
+  :ensure t
+  :commands restclient-mode)
+
+;; Dashboard
+(use-package dashboard
+  :ensure t
+  :diminish dashboard-mode
+  :init
+  (add-hook 'after-init-hook 'dashboard-refresh-buffer)
+  (add-hook 'dashboard-mode-hook 'my/dashboard-banner)
+  :config
+  ;; (setq dashboard-banner-logo-title "your custom text")
+  ;; (setq dashboard-startup-banner "/path/to/image")
+  ;; (setq dashboard-items '((recents  . 10)
+  ;;                         (bookmarks . 10)))
+  (setq dashboard-startup-banner 'logo)
+  ;; copy-pasted the logic of (dashboard-setup-startup-hook):
+  (add-hook 'after-init-hook (lambda ()
+			       ;; Display useful lists of items
+			       (dashboard-insert-startupify-lists)))
+  (add-hook 'emacs-startup-hook '(lambda ()
+				   (switch-to-buffer "*dashboard*")
+				   (goto-char (point-min))
+				   (redisplay))))
+
+;; Evil comments
+(use-package evil-commentary
+  :ensure t
+  :config
+  (evil-commentary-mode))
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Magit
+(use-package magit
+  :ensure t)
+
+(use-package evil-magit
+  :ensure t
+  :after (:all evil magit))
+
+;; Yaml
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.yml\\'"
+  :config
+  (add-hook 'yaml-mode-hook
+	    '(lambda ()
+	       (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
+
+;; Go test
+(use-package gotest
+  :ensure t
+  :after go-mode)
+
+;; Yasnippet
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
+;; Elpy
+(use-package elpy
+  :ensure t
+  :init
+  (setq elpy-rpc-python-command "python3")
+  (setq python-shell-interpreter "python3")
+  (setq python-shell-interpreter-args "-i")
+  :config (elpy-enable))
+
+;; Aggressive indent
+(use-package aggressive-indent
+  :ensure t
+  :hook (prog-mode . aggressive-indent-mode))
+
+;; ace link
+(use-package ace-link
+  :ensure t
+  :config
+  (ace-link-setup-default))
+
+;; link-help
+(use-package link-hint
+  :ensure t
+  :defer t)
+
+(use-package multi-term
+  :ensure t
+  :config
+  (setq multi-term-program "/bin/bash"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Functions and stuff ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun find-user-init-file ()
+  "Open the Emacs init file in the current window."
+  (interactive)
+  (find-file (locate-user-emacs-file "init.el")))
+
+(setq visible-bell nil
+      ring-bell-function 'flash-mode-line)
+
+(defun flash-mode-line ()
+  "Flash mode line."
+  (invert-face 'mode-line)
+  (run-with-timer 0.1 nil #'invert-face 'mode-line))
+
+(defun spacemacs/alternate-buffer (&optional window)
+  "Switch back and forth between current and last buffer in the current WINDOW."
+  (interactive)
+  (let ((current-buffer (window-buffer window)))
+    ;; if no window is found in the windows history, `switch-to-buffer' will
+    ;; default to calling `other-buffer'.
+    (switch-to-buffer
+     (cl-find-if (lambda (buffer)
+                   (not (eq buffer current-buffer)))
+                 (mapcar #'car (window-prev-buffers window)))
+     nil t)))
+
+;; https://www.reddit.com/r/emacs/comments/8jaflq/tip_how_to_use_your_dashboard_properly/
+(defun my/dashboard-banner ()
+  "Set a dashboard banner including information on package initialization time and garbage collections."
+  (setq dashboard-banner-logo-title
+        (format "Emacs ready in %.2f seconds: %d packages activated; %d garbage collections."
+                (float-time (time-subtract after-init-time before-init-time)) (length package-activated-list) gcs-done)))
+
+;; Swiper with symbol at point
+(defun swiper-at-point ()
+  "Call `counsel-grep-or-swiper' with symbol at point as initial value."
+  (interactive)
+  (counsel-grep-or-swiper
+   (when-let ((sym (symbol-at-point)))
+     (symbol-name sym))))
+
+;;;;;;;;;;;;;
+;; kmacros ;;
+;;;;;;;;;;;;;
+
+(fset 'go-wrap-if-err
+   [?^ ?i ?i ?f ?  ?e ?r ?r ?  ?! backspace ?: ?= ?  escape ?A ?\; ?  ?e ?r ?r ?  ?! ?= ?  ?n ?u ?i backspace backspace ?i ?l ?  ?\{ return ?r ?e ?t ?u ?r ?n ?  ?e ?r ?r return escape])
+
+;; Load customizations from a dedicated file
+(load custom-file)
